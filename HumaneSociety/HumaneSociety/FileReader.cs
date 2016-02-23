e13143dda5482a0490ff67e1ec25a3c5a400fa1f
cpetersend89 <cpetersend@gmail.com>
2016-02-23 15:47:23 -0600

    added classes, implemented login in read/write classes

---
+++ HumaneSociety/HumaneSociety/FileReader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumaneSociety
+{
+    public class FileReader
+    {
+        private readonly string _fileName;
+
+        public FileReader(string fileName)
+        {
+            _fileName = fileName;
+        }
+
+        public void ReadFromFile()
+        {
+            using (FileStream fs = new FileStream(_fileName, FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    sr.ReadToEnd();
+                }
+            }
+        }
+    }
+}
