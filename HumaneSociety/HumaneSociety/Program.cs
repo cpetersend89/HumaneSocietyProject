3e23608756b2c98580f41c5ccec998c985ecdb67
cpetersend89 <cpetersend@gmail.com>
2016-02-23 16:09:32 -0600

    added  a couple more classes

--- HumaneSociety/HumaneSociety/Program.cs
+++ HumaneSociety/HumaneSociety/Program.cs
@@ -10,13 +10,13 @@ namespace HumaneSociety
     {
         static void Main(string[] args)
         {
-            var fw = new FileWriter(@"../../test.txt");
-            fw.WriteToFile("Testing to see if this writes to the file.");
+            //var fw = new FileWriter(@"../../test.txt");
+            //fw.WriteToFile("Testing to see if this writes to the file.");
 
 
-            var fr = new FileReader(@"../../test.txt");
-            string testFr = fr.ReadFromFile();
-            Console.WriteLine(testFr);
+            //var fr = new FileReader(@"../../test.txt");
+            //string testFr = fr.ReadFromFile();
+            //Console.WriteLine(testFr);
 
 
             Console.ReadKey();
